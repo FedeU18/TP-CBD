c02453ea640e0c9cf68426bf4a7f7d58ca48e2fc
Federico <fedee.unates.2001@gmail.com>
2024-05-30 14:34:41 -0300

    tabla pasajeros creada con politicas de actualización y borrado

--- ejercicio2.sql
+++ ejercicio2.sql
@@ -1,2 +1,20 @@
 CREATE DATABASE viajes_de_placer;
 
+CREATE TABLE personas (
+  tipoDoc varchar(11) NOT NULL,
+  nroDocumento int(11) NOT NULL,
+  apellido varchar(20) NOT NULL,
+  nombre varchar(20) NOT NULL,
+  fechaNacimiento date NOT NULL,
+  telefono int(11) NOT NULL,
+  direccion varchar(50) NOT NULL,
+  PRIMARY KEY (tipoDoc, nroDocumento)
+)
+
+CREATE TABLE pasajeros (
+  tipoDoc varchar(11) NOT NULL,
+  nroDocumento int(11) NOT NULL,
+  situacionFiscal varchar(20) NOT NULL,
+  PRIMARY KEY (tipoDoc,nroDocumento),
+  FOREIGN KEY (tipoDoc,nroDocumento) REFERENCES `personas`(tipoDoc,nroDocumento) ON UPDATE cascade ON DELETE cascade
+)
