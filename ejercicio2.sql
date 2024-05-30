84a11fbe2d77b5bbb148d0450e1355ede8853ef3
Federico <fedee.unates.2001@gmail.com>
2024-05-30 14:37:59 -0300

    tabla choferes creada con sus politicas

--- ejercicio2.sql
+++ ejercicio2.sql
@@ -9,12 +9,27 @@ CREATE TABLE personas (
   telefono int(11) NOT NULL,
   direccion varchar(50) NOT NULL,
   PRIMARY KEY (tipoDoc, nroDocumento)
-)
+);
 
 CREATE TABLE pasajeros (
   tipoDoc varchar(11) NOT NULL,
   nroDocumento int(11) NOT NULL,
   situacionFiscal varchar(20) NOT NULL,
   PRIMARY KEY (tipoDoc,nroDocumento),
-  FOREIGN KEY (tipoDoc,nroDocumento) REFERENCES `personas`(tipoDoc,nroDocumento) ON UPDATE cascade ON DELETE cascade
-)
+  FOREIGN KEY (tipoDoc,nroDocumento) 
+    REFERENCES personas(tipoDoc,nroDocumento) 
+      ON UPDATE cascade 
+      ON DELETE cascade,
+);
+
+CREATE TABLE choferes (
+  tipoDoc varchar(11) NOT NULL,
+  nroDocumento int(11) NOT NULL,
+  legajo varchar(50) NOT NULL,
+  categoria varchar(50) NOT NULL,
+  PRIMARY KEY (tipoDoc,nroDocumento),
+  FOREIGN KEY (tipoDoc,nroDocumento)
+   REFERENCES personas(tipoDoc,nroDocumento) 
+    ON UPDATE cascade 
+    ON DELETE cascade,
+);
